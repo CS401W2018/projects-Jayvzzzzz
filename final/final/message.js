207bd410a9a7d4b71b2e145ed430fa04ce87d79d
JV Vicente <jv.vicente999@gmail.com>
2025-04-11 05:43:57 -1000

    try

--- final/final/message.js
+++ final/final/message.js
@@ -1,4 +1,4 @@
-document.getElementById('myForm').addEventListener('submit',function(event){
+document.getElementById('messages').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const firstname = document.getElementById('firstname').value;
@@ -12,19 +12,25 @@ document.getElementById('myForm').addEventListener('submit',function(event){
         email: email,
         message: message
     };
-    console.log(formData)
+
+    console.log("Form Data:", formData);
+
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "submit.json", true);
-    xhr.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+    xhr.open("POST", "submit.json", true); // Use POST instead of GET
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            document.getElementById("messages").innerHTML = response.message ;
-            document.getElementById("myForm").innerHTML = "";
-        } else if (xhr.readystate === 4) {
-            alert('Error submitting form.');
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                document.getElementById("message").innerHTML = response.message;
+                document.getElementById("myForm").reset(); 
+            } else {
+                alert('Error submitting form.');
+            }
         }
     };
-    console.log("Sending")
+
+    console.log("Sending...");
     xhr.send(JSON.stringify(formData));
 });
