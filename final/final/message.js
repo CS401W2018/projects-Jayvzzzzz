b49cc0dd01f3ac12c4d42736bb4e1cc2efbee2eb
JV Vicente <jv.vicente999@gmail.com>
2025-04-11 05:47:50 -1000

    try

--- final/final/message.js
+++ final/final/message.js
@@ -16,7 +16,7 @@ document.getElementById('messages').addEventListener('submit', function(event) {
     console.log("Form Data:", formData);
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "submit.json", true); // Use POST instead of GET
+    xhr.open("GET", "submit.json", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
     xhr.onreadystatechange = function () {
