bf56b5613765ebc2b2b26ba1d033992e68893eac
JV Vicente <jv.vicente999@gmail.com>
2025-04-11 06:37:28 -1000

    try

--- final/final/messages.js
+++ final/final/messages.js
@@ -23,7 +23,7 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
                 const response = JSON.parse(xhr.responseText);
-                document.getElementById("messages").innerHTML = response.message;
+                document.getElementById("message").innerHTML = response.message;
                 document.getElementById("myForm").reset(); 
             } else {
                 alert('Error submitting form.');
