08cb1c1cf27f7375b93736daea2e657a28b1427a
JV Vicente <jv.vicente999@gmail.com>
2025-04-10 23:03:35 -1000

    try

---
+++ final/final/message.js
@@ -0,0 +1,31 @@
+document.getElementById('myForm').addEventListener('submit',function(event){
+    event.preventDefault();
+
+    const fullname = document.getElementById('fullname').value;
+    const password = document.getElementById('pass').value;
+    const email = document.getElementById('email').value;
+    const age = document.getElementById('age').value;
+    const state = document.getElementById('state').value;
+
+    const formData = {
+        fullname: fullname,
+        email: email,
+        password: pass,
+        age: age
+    };
+    console.log(formData)
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            document.getElementById("message").innerHTML = response.message ;
+            document.getElementById("myForm").innerHTML = "";
+        } else if (xhr.readystate === 4) {
+            alert('Error submitting form.');
+        }
+    };
+    console.log("Sending")
+    xhr.send(JSON.stringify(formData));
+});
